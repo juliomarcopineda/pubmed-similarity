c9cd3ee0d778ca3d83f9529ca5f154685ab7018a
juliomarcopineda <juliomarco.pineda@nih.gov>
2019-08-08 12:52:42 -0400

    add comments and reformat code

--- mongo_load.py
+++ mongo_load.py
@@ -1,21 +1,71 @@
-from data.mongo_provider import MongoProvider
-from pathlib import Path
+"""Loads publication data from a CSV file to a MongoDB instance with the collection name: 'publications'.
+"""
+
 import csv
+from pathlib import Path
 
+from data.mongo_fields import Publications
+from data.mongo_provider import MongoProvider
 
-publications = MongoProvider(Path.home() / '.dsca' / 'app.config').get_publications_collection()
+collection = MongoProvider(Path.home() / '.dsca' / 'app.config').get_publications_collection()
 
 
 def construct_mongo_doc(row):
-    pass
+    """Given a row or record from the publication data, construct a dictionary to be inserted to
+    the publications collection
+
+    :param row: row/record of publication data
+    :return: dict document of publication data
+    """
+
+    doc = {}
+
+    pmid = row['pmid']
+    if pmid is not None:
+        doc[Publications.PMID.mongo] = int(pmid)
+
+    year = row['year']
+    if year is not None:
+        doc[Publications.PUBLICATION_YEAR.mongo] = int(year)
+
+    journal = row['journal']
+    if journal is not None and journal != '':
+        doc[Publications.JOURNAL.mongo] = journal
+
+    title = row['title']
+    if title is not None and title != '':
+        doc[Publications.TITLE.mongo] = title
+
+    authors_string = row['authors']
+    if authors_string is not None and authors_string != '':
+        doc[Publications.AUTHORS.mongo] = authors_string.split(';')
+
+    abstract = row['abstract']
+    if abstract is not None and abstract != '':
+        doc[Publications.ABSTRACT.mongo] = abstract
+
+    return doc
 
 
 def main():
+    print('Inserting PubMed records to MongoDB...')
+    print('Dropping publications collection...')
+    collection.drop()
+
+    status = 0
     data_path = Path.home() / '2018_cancer.csv'
     with open(data_path, mode='r', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.DictReader(csvfile)
         for row in reader:
+            status += 1
+            if status % 5000 == 0:
+                print('STATUS:', status)
+
             doc = construct_mongo_doc(row)
+            collection.insert_one(doc)
+
+    print('STATUS:', status)
+    print('Done.')
 
 
 if __name__ == '__main__':
