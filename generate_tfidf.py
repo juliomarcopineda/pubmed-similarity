5c98d9b39105deb85d5a64ea7bc3e7fa3d835a0f
juliomarcopineda <juliomarcopineda@gmail.com>
2019-08-10 14:36:12 -0400

    limit tfidf calculation to 2018 and 2019

--- generate_tfidf.py
+++ generate_tfidf.py
@@ -21,7 +21,9 @@ def normalize_vector(vector):
 
 
 def main():
-    docs = collection.find({}, {Publications.CLEAN_TEXT.mongo: 1})
+    docs = collection.find({'$or': [{Publications.PUBLICATION_YEAR.mongo: 2018},
+                                    {Publications.PUBLICATION_YEAR.mongo: 2019}]},
+                           {Publications.CLEAN_TEXT.mongo: 1})
 
     print('Determining document and term frequencies...')
     doc_term_freq = {}
